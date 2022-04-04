cf6755782c6d32703583ed02a35782184cbe2732
mishakura <michaelsojo22@gmail.com>
2022-04-04 10:00:33 -0300

    Api key variable

--- axios-API-CountryInfoGenerator/App.js
+++ axios-API-CountryInfoGenerator/App.js
@@ -7,6 +7,8 @@ import Weather from "./components/Weather"
 
 
 const App = () =>{
+  const api_key = process.env.REACT_APP_API_KEY
+  console.log(process.env)
   
   const [country, setCountry] = useState([]);
   const [formInput, setformInput] = useState("");
@@ -36,7 +38,7 @@ const App = () =>{
     let latitude = filteredCountry[0].capitalInfo.latlng[0]
     let longitude = filteredCountry[0].capitalInfo.latlng[1]
     axios
-    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=9a1ee4b95d0e532b648926b0256f12b2&units=metric`)
+    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}&units=metric`)
     .then(response => {
       
       const data = response.data
