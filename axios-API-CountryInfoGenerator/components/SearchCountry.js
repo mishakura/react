db8564379f84896683097ba62573b9639caf9ca4
mishakura <79343312+mishakura@users.noreply.github.com>
2022-04-08 10:44:41 -0300

    Delete axios-API-CountryInfoGenerator directory

--- axios-API-CountryInfoGenerator/components/SearchCountry.js
+++
@@ -1,9 +0,0 @@
-const SearchCountry = (props) =>{
-    return(
-        <form >
-            Find country: <input value = {props.val}  onChange ={props.formChange}/>
-
-        </form>
-    )
-}
- export default SearchCountry;
